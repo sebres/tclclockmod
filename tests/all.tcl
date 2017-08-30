322a4875cb468278050af3f36f8793315dd93b6c
sebres <info@sebres.de>
2017-08-30 11:57:49 +0200

    install newest tcl version - trusty has too old tcl version (8.6.0); before starting test-cases check we've loaded tclclockmod

--- tests/all.tcl
+++ tests/all.tcl
@@ -3,6 +3,8 @@ puts "Testing with [info patchlevel] from [info nameofexecutable] ..."
 puts "Load library ..."
 # load library:
 source [file dirname [file dirname [info script]]]/lib/loader.tcl
+# test it is really new version:
+clock format -now -format "%Es" -gmt 1
 
 if {[catch {
   cd $::env(TEMP)
